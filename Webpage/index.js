6fbbc77ac0f175a74cb3c2d908349473722d6b0c
Harshith.B.N <harshith0998@gmail.com>
2024-10-25 21:49:36 +0530

    Javascript intro

---
+++ Webpage/index.js
@@ -0,0 +1,47 @@
+function sample(){
+    if(true){
+    var x = 100
+    let y = 200
+    const z = 300
+
+    console.log(x)
+    console.log(y)
+    console.log(z)
+
+    var x = 150
+    console.log(x)
+    }}
+//sample()
+
+
+function sample1(){
+    var x = 'Hello'
+    console.log(x[0])
+
+    var myarray = [100,"Hello",9.05];
+    console.log("Before:",myarray[0])
+    myarray[0] = 500
+    console.log("After",myarray[0])
+
+    console.log(myarray)
+
+    myarray.push('NewItem')
+    console.log(myarray)
+
+    myarray[6] = 656
+    console.log(myarray)
+
+    var array_2d = [[20,"First",0.21],[10,"Second"]]
+    console.log(array_2d)
+    console.log(array_2d[1])
+    console.log(array_2d[1][1])
+}
+//sample1()
+
+function sample2(p1,p2,p3){
+    var new_item = ""
+    new_item = p1+p2+p3
+    return new_item
+}
+
+console.log(sample2("Hello ","World"))
